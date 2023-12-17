f9ddfade4af9cb661ce57ca3c2a5f3ea915a5d1c
hasanbriq <hasan@br.iq>
2023-12-18 01:03:20 +0530

    adds FAQ section to homepage

--- index.js
+++ index.js
@@ -34,6 +34,32 @@ document.addEventListener("DOMContentLoaded", () => {
   navbarToggler.addEventListener("click", function (e) {
     e.target.children[0].classList.toggle("active");
   });
+
+  // Add minus icon for collapse element which is open by default
+  $(".collapse.show").each(function () {
+    $(this)
+      .prev(".accordion-header")
+      .find(".fa")
+      .addClass("fa-minus")
+      .removeClass("fa-plus");
+  });
+
+  // Toggle plus minus icon on show hide of collapse element
+  $(".collapse")
+    .on("show.bs.collapse", function () {
+      $(this)
+        .prev(".accordion-header")
+        .find(".fa")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+    })
+    .on("hide.bs.collapse", function () {
+      $(this)
+        .prev(".accordion-header")
+        .find(".fa")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    });
 });
 
 // owl carousel 
